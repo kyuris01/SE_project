180f9feecb81152f690668d1ade3fba7bb8b2f6e
Chris Ahn <chrisahn1007@gmail.com>
2024-11-22 23:29:23 +0900

    left나 right로 전환후에는 한 phase동안은 직진하도록 inputdata를 수정

--- rvc/tester.c
+++ rvc/tester.c
@@ -5,37 +5,30 @@
 #define ITERATION_NUM 10
 
 void tester();
-// char*inputData[ITERATION_NUM];
-bool positionSequence[ITERATION_NUM][4];
+bool positionSequence[ITERATION_NUM][4]; //일련의 obstacle위치와 dust의 존재유무를 저장하는 이차원배열
 
 
 void tester() {
     
-    bool positionAtTheMoment;
 
     srand(time(NULL));
     
     for (int i = 0; i< ITERATION_NUM ; i++) {
+        if ((positionSequence[i-1][1] == 1) || (positionSequence[i-1][2] == 1)) {
+            positionSequence[i][0] = 0;
+            for (int k = 1;k < 4; k++) {
+                positionSequence[i][k] = rand() % 2;
+            }
+            continue;
+        }
         for(int j = 0; j < 4 ; j++) {
-            int randomExistence;
-            randomExistence = rand() % 2;
-            positionSequence[i][j] = randomExistence;
+            // int randomExistence;
+            // randomExistence = rand() % 2;
+            positionSequence[i][j] = rand() % 2;
         }
 
     }
 
-    // for (int i = 0; i < ITERATION_NUM; i++) {
-    //     inputData[i] = (char*)malloc(5 * sizeof(char)); // 문자열 저장 공간 할당 (4자리 + '\0')
-    //     for (int j = 0; j < 4; j++) {
-    //         inputData[i][j] = positionSequence[i][j] ? '1' : '0'; // bool 값을 문자로 변환
-    //     }
-    //     inputData[i][4] = '\0'; // null 문자 추가
-    // }
-
-    // for (int i = 0; i < ITERATION_NUM; i++) {
-    //     printf("%s\n", inputData[i]);
-    //     free(inputData[i]); // 동적 메모리 해제
-    // }
 
     for (int i = 0; i< ITERATION_NUM ; i++) {
         for(int j = 0; j < 4 ; j++) {
